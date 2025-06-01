a0464ee9ecd2a964ab623ef85e4dcb12335dbece
Aleksey Zhbankov <ipzhbankov@yandex.ru>
2025-06-01 16:41:07 +0300

    nadezhda

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))         # ← теперь это int
+CHAT_ID = int(os.getenv("CHAT_ID"))         # Числовой ID канала, например: -1002649471666
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OWNER_ID = int(os.getenv("OWNER_ID"))       # ← тоже int
+OWNER_ID = int(os.getenv("OWNER_ID"))       # Числовой Telegram ID владельца, например: 407721399
+
 
