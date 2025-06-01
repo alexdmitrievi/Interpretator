92edde29228febe1cc98e8150ab5c60967da5222
Aleksey Zhbankov <ipzhbankov@yandex.ru>
2025-06-01 20:19:06 +0300

    ebana

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-CHAT_ID = int(os.getenv("CHAT_ID"))
-OWNER_ID = int(os.getenv("OWNER_ID"))
+CHAT_ID = os.getenv("CHAT_ID")
+OWNER_ID = os.getenv("OWNER_ID")
 
