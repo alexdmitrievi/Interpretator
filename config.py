d50cf34de82498a8cb3753af77a1fe31ef239947
Aleksey Zhbankov <ipzhbankov@yandex.ru>
2025-06-01 20:38:11 +0300

    kogda esli ne seichas

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-CHAT_ID = os.getenv("CHAT_ID")
-OWNER_ID = os.getenv("OWNER_ID")
+CHAT_ID = int(os.getenv("CHAT_ID"))
+OWNER_ID = int(os.getenv("OWNER_ID"))
 
 print("[DEBUG] TELEGRAM_TOKEN =", TELEGRAM_TOKEN)
 print("[DEBUG] OPENAI_API_KEY =", OPENAI_API_KEY)
-print("[DEBUG] CHAT_ID =", CHAT_ID)
-print("[DEBUG] OWNER_ID =", OWNER_ID)
+print("[DEBUG] CHAT_ID =", CHAT_ID, type(CHAT_ID))
+print("[DEBUG] OWNER_ID =", OWNER_ID, type(OWNER_ID))
+
 
 
