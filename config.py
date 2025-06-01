8d382c8b8cf16700e8794fad0e915d7869fab489
Aleksey Zhbankov <ipzhbankov@yandex.ru>
2025-06-01 20:25:40 +0300

    togo rot ebal

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
-import os
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 CHAT_ID = os.getenv("CHAT_ID")
 OWNER_ID = os.getenv("OWNER_ID")
+
+print("[DEBUG] TELEGRAM_TOKEN =", TELEGRAM_TOKEN)
+print("[DEBUG] OPENAI_API_KEY =", OPENAI_API_KEY)
+print("[DEBUG] CHAT_ID =", CHAT_ID)
+print("[DEBUG] OWNER_ID =", OWNER_ID)
+
 
